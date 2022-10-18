76d76aa38faa8f7054604fd67b6c066d76b4fbd9
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-18 18:13:47 +0200

    Starting TS files

---
+++ src/components/component.ts
@@ -0,0 +1,29 @@
+export abstract class Component {
+    renderInner(selector: string, template: string) {
+        const element = document.querySelector(selector);
+        if (element === null) {
+            return false;
+        }
+        element.innerHTML = template;
+        return true;
+    }
+
+    renderOuter(selector: string, template: string) {
+        const element = document.querySelector(selector);
+        if (element === null) {
+            return false;
+        }
+        element.outerHTML += template;
+        return true;
+    }
+
+    renderAdd(selector: string, template: string) {
+        const element = document.querySelector(selector);
+        if (element === null) {
+            return false;
+        }
+
+        element.innerHTML = template;
+        return true;
+    }
+}
